5c4ea34e2bbc0cb87af623afd45639b22b61c213
EduardoPalharesRealePereira <dudureale04@gmail.com>
2026-05-19 10:33:26 -0300

    pla

---
+++ Ibmec/POO/DataEHora/src/aula1/Main.java
@@ -0,0 +1,21 @@
+package aula1;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+	
+	LocalDateTime agora =  LocalDateTime.now();
+	System.out.println("Agora: " + agora);
+	LocalDateTime dt = LocalDateTime.of(2025, 11, 10, 17, 30);
+	System.out.println("Data: " + dt);
+	LocalDate hoje = LocalDate.now();
+	System.out.println("Hoje: " + hoje);
+		
+	}
+
+}
